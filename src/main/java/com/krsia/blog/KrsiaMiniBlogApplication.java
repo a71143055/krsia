059e7be398dc7802694e5ef7464af91e58163ab1
a71143055 <a71143055@gmail.com>
2025-11-23 17:52:50 +0900

    KRSIA

---
+++ src/main/java/com/krsia/blog/KrsiaMiniBlogApplication.java
@@ -0,0 +1,13 @@
+package com.krsia.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrsiaMiniBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrsiaMiniBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krsia/blog/KrsiaMiniBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krsia.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrsiaMiniBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
