26e1979e3ff0ad5c5d0c2272e08bcc5287a6f56b
a71143055 <a71143055@gmail.com>
2025-11-23 18:31:03 +0900

    KRSIA

---
+++ src/main/java/com/krsia/blog/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.krsia.blog.service;
+
+import com.krsia.blog.entity.User;
+import com.krsia.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User u = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(u.getEmail())
+                .password(u.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
